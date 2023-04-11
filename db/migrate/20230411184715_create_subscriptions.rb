87f2ffee3e2c2a1184c35a1fc18fea9bdda53446
Drew Layton <dlayton66@gmail.com>
2023-04-11 12:52:05 -0600

    Create and run migrations

---
+++ db/migrate/20230411184715_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.decimal :price
+      t.integer :frequency
+      t.references :tea, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
