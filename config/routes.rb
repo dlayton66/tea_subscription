2a240be50293d1cf93177d89a6013d8db41d4d02
Drew Layton <dlayton66@gmail.com>
2023-04-11 20:53:44 -0600

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: [] do
+        post '/subscriptions/:subscription_id', to: 'customer/subscriptions#create', as: 'subscription'
+        resources :subscriptions, only: %i[index destroy], controller: 'customer/subscriptions'
+      end
+    end
+  end
 end
