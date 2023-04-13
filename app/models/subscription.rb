eac2108ef3c4530ca0115a70becec82ac767cd04
Drew Layton <dlayton66@gmail.com>
2023-04-13 13:54:21 -0600

    Add note for question for Brian

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,6 @@ class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
 
-  attribute :status, default: 1
+  attribute :status, default: 1 # why does this show up as covered in unit tests?
   enum status: [:cancelled, :active]
 end
